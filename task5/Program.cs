b94f18d0d7d7963158a54d6a68bda0ef751937c7
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-05 22:14:41 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,6 @@
+﻿//напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа 
+Console.WriteLine("Введите трезначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = number % 10;
+Console.WriteLine(i);
+
