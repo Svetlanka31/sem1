8999bb88a3355d97de7ba00a0676f61cee601c92
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-13 16:10:32 +0300

    added task16

---
+++ task16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+double count = 1;
+while (count <= number)
+{
+    Console.WriteLine(Math.Pow(count,2));
+ count++;
+ }
+
+
