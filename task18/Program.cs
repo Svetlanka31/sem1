69627e27cc403350431577ffa927672140b36828
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-17 22:44:37 +0300

    added task18

---
+++ task18/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает число А и выдает сумму чисел от 1 до А
+Console.WriteLine("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i<= number; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine($"Результата работы программы = {sum}");
