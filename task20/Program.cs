be1ebc311107bd6124ac705d5b63cbc925639446
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-19 15:47:28 +0300

    added second variant

--- task20/Program.cs
+++ task20/Program.cs
@@ -2,12 +2,23 @@
 //4 -> 24 
 //5 -> 120
 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
-for ( int i=1; i <=number;i++)
-{
-    count = count *i;
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// for ( int i=1; i <=number;i++)
+// {
+//     count = count *i;
  
+// }
+//  Console.WriteLine( count);
+
+// 2 способ
+
+Console.WriteLine("Enter number N:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count =1;
+for( int i = 1; i <= number; i++)
+{
+    count = count*i;
 }
- Console.WriteLine( count);
+Console.WriteLine(count);
