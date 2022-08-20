5f7b37f7878087a3014e9bd5b06ee1197e9d85d9
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-20 21:05:41 +0300

    added task25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените 
+//на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+int[]array = new int[4];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10,11);
+}
+Console.WriteLine("["+ string.Join(", ", array) + "]");
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+
+Console.WriteLine("["+ string.Join(", ", array) + "]");
+ 
