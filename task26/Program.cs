00177621f1b7f776e1aaf98f06a42ab578807a2e
Svetlana <kozhevnikova-svetlana85@bk.ru>
2022-08-20 21:22:55 +0300

    added task26

---
+++ task26/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, 3] -> да
+
+int[]array = new int[12];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10,11);
+}
+Console.WriteLine("["+ string.Join(", ", array) + "]");
+  Console.WriteLine("Введите целое число: ");
+  int number = Convert.ToInt32(Console.ReadLine());
+  int j = 0;
+  for (int i =0; i < array.Length; i++)
+  {
+    if (array[i]==number)
+    {
+     j++;   
+    }
+  }
+    if(j > 0)
+    {
+    Console.WriteLine("Да");
+    }  
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+  
